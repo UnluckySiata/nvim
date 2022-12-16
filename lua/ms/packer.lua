ea19c38893c57150b518a564e92103525b72dcac
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2022-12-16 23:35:51 +0100

    added autopairing for bracket closing

--- lua/ms/packer.lua
+++ lua/ms/packer.lua
@@ -52,5 +52,10 @@ return require('packer').startup(function(use)
       }
   }
 
+  use {
+      "windwp/nvim-autopairs",
+      config = function() require("nvim-autopairs").setup {} end
+  }
+
 
 end)
