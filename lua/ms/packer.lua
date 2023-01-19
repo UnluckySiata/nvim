7173f709d7847de2a2d202484365bd3169b0cf0f
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2023-01-19 16:14:51 +0100

    Added packer bootstraping

--- lua/ms/packer.lua
+++ lua/ms/packer.lua
@@ -1,64 +1,79 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        print 'Installing plugin manager..'
+        vim.cmd.packadd('packer.nvim')
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  
-  -- Colorschemes
-  use('folke/tokyonight.nvim')
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine'
-  })
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.0',
-      -- or                            , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    -- Colorschemes
+    use('folke/tokyonight.nvim')
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine'
+    })
 
-  use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
-  }
+    use('kyazdani42/nvim-web-devicons')
 
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('ggandor/leap.nvim')
-  use('kdheepak/lazygit.nvim')
-  use('numToStr/Comment.nvim')
-  use('kylechui/nvim-surround')
-  use('vale1410/vim-minizinc')
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use {
-      'VonHeikemen/lsp-zero.nvim',
-      requires = {
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},
-          {'williamboman/mason.nvim'},
-          {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            pcall(require('nvim-treesitter.install').update { with_sync = true })
+        end
+    }
 
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp'},
-          {'hrsh7th/cmp-buffer'},
-          {'hrsh7th/cmp-path'},
-          {'saadparwaiz1/cmp_luasnip'},
-          {'hrsh7th/cmp-nvim-lsp'},
-          {'hrsh7th/cmp-nvim-lua'},
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('ggandor/leap.nvim')
+    use('kdheepak/lazygit.nvim')
+    use('numToStr/Comment.nvim')
+    use('kylechui/nvim-surround')
+    use('windwp/nvim-autopairs')
+    use('vale1410/vim-minizinc')
 
-          -- Snippets
-          {'L3MON4D3/LuaSnip'},
-          {'rafamadriz/friendly-snippets'},
-      }
-  }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-  use {
-      "windwp/nvim-autopairs",
-      config = function() require("nvim-autopairs").setup {} end
-  }
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
 
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+
+    if packer_bootstrap then
+        print 'Installing plugins..'
+        require('packer').sync()
+    end
 
 end)
