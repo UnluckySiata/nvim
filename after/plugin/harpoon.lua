136f8b82bb21c65d142daac922a195e8608fa3ee
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2023-01-19 15:20:14 +0100

    Better keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,9 +5,9 @@ local term = require("harpoon.term")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 
-vim.keymap.set("n", "<leader>bi", ui.toggle_quick_menu)
-vim.keymap.set("n", "<leader>bp", function() ui.nav_prev() end)
-vim.keymap.set("n", "<leader>bn", function() ui.nav_next() end)
+vim.keymap.set("n", "<leader>hi", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>hp", function() ui.nav_prev() end)
+vim.keymap.set("n", "<leader>hn", function() ui.nav_next() end)
 
 for i=1,9 do
     vim.keymap.set("n", string.format("<leader>%s", i), function() ui.nav_file(i) end)
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,24 @@
 local builtin = require('telescope.builtin')
+
+-- Buffers
+vim.keymap.set('n', '<leader>bi', builtin.buffers, {})
+
+-- Project
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pgf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+
+-- LSP
+vim.keymap.set('n', '<leader>lr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>li', builtin.lsp_implementations, {})
+vim.keymap.set('n', '<leader>lgd', builtin.lsp_definitions, {})
+vim.keymap.set('n', '<leader>ld', builtin.diagnostics, {})
+
+-- Treesitter
+vim.keymap.set('n', '<leader>ts', builtin.treesitter, {})
+
+-- Git
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+
