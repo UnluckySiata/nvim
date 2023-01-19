24e98b89f1edf628a03133cf7af380a6490876d6
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2023-01-19 18:19:14 +0100

    Initial neovide support

--- lua/ms/init.lua
+++ lua/ms/init.lua
@@ -1,3 +1,4 @@
 require('ms.set')
 require('ms.remap')
 require('ms.packer')
+require('ms.neovide')
---
+++ lua/ms/neovide.lua
@@ -0,0 +1,10 @@
+if vim.g.neovide == nil then
+    return
+end
+
+vim.g.neovide_scale_factor = 1.1
+vim.g.neovide_transparency = 0.8
+vim.g.neovide_hide_mouse_when_typing = false
+vim.g.neovide_scroll_animation_length = 0.1
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_remember_window_size = true
