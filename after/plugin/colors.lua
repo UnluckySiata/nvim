32fd46a8f9b465e9d6a4264e0d76102d11a606fa
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2023-01-20 21:09:55 +0100

    Proper theme setup

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,15 +2,21 @@ if not pcall(require, "rose-pine") then
     return
 end
 
+require("rose-pine").setup({
+    dark_variant = "main",
+    disable_background = true,
+    disable_italics = true
+})
+
 function ColorSet(color)
     color = color or "rose-pine"
     vim.cmd.colorscheme(color)
 
     -- Transparency
-    --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 end
 
--- ColorSet('tokyonight-night')
+-- ColorSet("tokyonight-night")
 ColorSet()
