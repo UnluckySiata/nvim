33d6bccf8394a0297181797b8d4eb4b5e8234de6
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2023-05-05 00:42:45 +0200

    Simplified lualine look

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -3,6 +3,14 @@ if not ok then return end
 
 lualine.setup({
     options = {
-        theme = "seoul256"
-    }
+        theme = "seoul256",
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = { "filetype" },
+        lualine_z = { "location" },
+    },
 })
