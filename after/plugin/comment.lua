dd1906315f4116af45e6fdb06215ffe759048de2
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2023-08-20 15:27:47 +0200

    fixup! Gitsigns integration
    oops, useless changes

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,20 +1,4 @@
 local ok, comment = pcall(require, "Comment")
 if not ok then return end
 
-comment.setup {
-    toggler = {
-        line = "zcc",
-        block = "zbc"
-    },
-
-    opleader = {
-        line = "zc",
-        block = "zb"
-    },
-
-    extra = {
-        above = "zcO",
-        below = "zco",
-        eol = "zca"
-    }
-}
+comment.setup {}
