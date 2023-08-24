a5e203cc22099a62ae33c89a5e3bb08471972459
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2023-08-24 23:25:54 +0200

    Visual substitution

--- lua/ms/keymaps.lua
+++ lua/ms/keymaps.lua
@@ -30,4 +30,6 @@ vim.keymap.set("n", "[Q", ":cfirst<CR>", { silent = true })
 vim.keymap.set("n", "]Q", ":clast<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("v", "<leader>s", "\"sy:%s/<C-r>s//gI<Left><Left><Left>")
+
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
