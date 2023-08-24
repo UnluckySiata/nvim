335ac010d4cac1453d9251ad1cf574367306ebc1
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2023-08-24 21:53:09 +0200

    Borders for lsp results

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -83,6 +83,16 @@ mason_lspconfig.setup_handlers({
     end
 })
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+    vim.lsp.handlers.hover,
+    { border = "rounded" }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.hover,
+    { border = "rounded" }
+)
+
 vim.diagnostic.config({
     virtual_text = false,
     inlay_hints = true,
