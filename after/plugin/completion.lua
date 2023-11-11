08e8e5f66cc8cf4bb2223ea73a028c4db479f87a
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2023-11-11 17:13:24 +0100

    cmp improvement

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -3,27 +3,31 @@ if not ok then return end
 
 local lspkind = require("lspkind")
 
-cmp.setup({
-    mapping = {
-        ['<c-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<c-f>'] = cmp.mapping.scroll_docs(4),
-        ['<s-tab>'] = cmp.mapping.select_prev_item(),
-        ['<tab>'] = cmp.mapping.select_next_item(),
-        ['<c-y>'] = cmp.mapping.confirm({
+cmp.setup {
+    mapping = cmp.mapping.preset.insert {
+        ["<c-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<c-f>"] = cmp.mapping.scroll_docs(4),
+        ["<c-e>"] = cmp.mapping.close(),
+        ["<s-tab>"] = cmp.mapping.select_prev_item(),
+        ["<tab>"] = cmp.mapping.select_next_item(),
+        ["<c-y>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
         }),
         ["<c-space>"] = cmp.mapping.complete(),
     },
 
-    sources = {
+    sources = cmp.config.sources {
         { name = "nvim_lua" },
         { name = "nvim_lsp" },
         { name = "path" },
         { name = "luasnip" },
         { name = "neorg" },
-        { name = "crates" },
         { name = "buffer", keyword_length = 4 },
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
 
     snippet = {
@@ -36,17 +40,23 @@ cmp.setup({
         format = lspkind.cmp_format({
             with_text = true,
             maxwidth = 40,
-            ellipsis_char = '..',
+            ellipsis_char = "..",
             menu = {
                 buffer = "[buf]",
                 nvim_lsp = "[LSP]",
                 nvim_lua = "[api]",
                 path = "[path]",
                 neorg = "[neorg]",
-                crates = "[crates]",
                 luasnip = "[snip]",
             }
         })
     },
+}
+
+cmp.setup.cmdline({ "/", "?" }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "buffer" }
+    }
 })
 
