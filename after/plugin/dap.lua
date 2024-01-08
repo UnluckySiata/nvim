ab662bb6116a2947db0ba447e4816fca0e9a547d
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-01-08 23:08:30 +0100

    more configs for c/c++/rust + cleanup

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -4,7 +4,21 @@ if not ok then return end
 local dapui = require("dapui")
 local dapvt = require("nvim-dap-virtual-text")
 dapui.setup()
-dapvt.setup { }
+dapvt.setup {}
+
+
+-- requires gdb 14 or higher
+dap.adapters.gdb = {
+    type = "executable",
+    command = "gdb",
+    args = { "-i", "dap" },
+}
+
+dap.adapters.rust_gdb = {
+    type = "executable",
+    command = "rust-gdb",
+    args = { "-i", "dap" },
+}
 
 dap.adapters.codelldb = {
     type = "server",
@@ -12,7 +26,7 @@ dap.adapters.codelldb = {
 
     executable = {
         command = vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/adapter/codelldb",
-        args = {"--port", "${port}"},
+        args = { "--port", "${port}" },
     },
 
     lldb = {
@@ -21,29 +35,36 @@ dap.adapters.codelldb = {
 
 }
 
-dap.adapters.c = dap.adapters.codelldb
-dap.adapters.cpp = dap.adapters.codelldb
-dap.adapters.rust = dap.adapters.codelldb
 
-dap.configurations.c = {
-  {
-    name = "Launch file",
-    type = "codelldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = function ()
-        local input = vim.fn.input("args: ")
-        return vim.split(input, " ")
-    end
-  },
-}
+local configs = {}
+local function mk_config(type)
+    return {
+        {
+            name = "Launch",
+            type = type,
+            request = "launch",
+            program = function()
+                return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = false,
+            args = function()
+                local input = vim.fn.input("args: ")
+                return vim.split(input, " ")
+            end
+        },
+    }
+end
 
-dap.configurations.cpp = dap.configurations.c
-dap.configurations.rust = dap.configurations.c
+configs.codelldb = mk_config("codelldb")
+configs.gdb = mk_config("gdb")
+configs.rust_gdb = mk_config("rust_gdb")
+
+
+dap.configurations.c = configs.codelldb
+dap.configurations.cpp = configs.codelldb
+dap.configurations.rust = configs.codelldb
+
 
 vim.keymap.set("n", "<F5>", dap.continue)
 vim.keymap.set("n", "<F10>", dap.step_over)
@@ -53,4 +74,3 @@ vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
 vim.keymap.set("n", "<leader>dr", dap.repl.open)
 vim.keymap.set("n", "<leader>dt", dapui.toggle)
 vim.keymap.set("n", "<leader>dv", ":DapVirtualTextToggle<CR>", { silent = true })
-
