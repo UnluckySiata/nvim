5b1006c03bd5f906a030299442a9eee7f80f2280
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-02-16 16:45:37 +0100

    disable cmp for : prompt

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -77,13 +77,3 @@ cmp.setup.cmdline({ "/", "?" }, {
         { name = "buffer" }
     }
 })
-
-cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" }
-  }, {
-    { name = "cmdline" }
-  })
-})
-
