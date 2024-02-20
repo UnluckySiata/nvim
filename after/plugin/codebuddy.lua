d21edd0e2a034f2ed8efaec860fa68de65d337f6
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-02-20 18:36:56 +0100

    initial setup for new codebuddy

--- after/plugin/codebuddy.lua
+++ after/plugin/codebuddy.lua
@@ -1,47 +1,49 @@
 local ok, cb = pcall(require, "codebuddy")
 if not ok then return end
 
-cb:setup {
-    commands = {
-        c = {
-            build = "clang-17 -Wall -g -o {file} {file_path}",
-            run = "./{file}"
-        },
-        cpp = {
-            build = "clang++-17 -g -o {file} -std=c++2b -stdlib=libc++ {file_path}",
-            run = "./{file}"
-        },
-        ex = {
-            build = "mix compile",
-            run = "iex -S mix"
-        },
-        erl = {
-            build = "erlc -o out {file_path}",
-            run = "erl -pa out"
-        },
-        py = {
-            run = "python3 {file_path}"
-        },
-        rs = {
-            build = "cargo build",
-            run = "cargo run"
-        },
-        go = {
-            build = "go build .",
-            run = "go run ."
-        },
-        java = {
-            build = "gradle build",
-            run = "gradle run"
-        },
+local commands = {
+    c = {
+        build = "clang-17 -Wall -g -o {file} {file_path}",
+        run = "./{file}"
     },
-
-    term = {
-        start_insert = false,
-        no_number = true,
+    cpp = {
+        build = "clang++-17 -g -o {file} -std=c++2b -stdlib=libc++ {file_path}",
+        run = "./{file}",
+        xd = "./{file}"
+    },
+    ex = {
+        build = "mix compile",
+        run = "iex -S mix"
+    },
+    erl = {
+        build = "erlc -o out {file_path}",
+        run = "erl -pa out"
+    },
+    py = {
+        run = "python3 {file_path}"
+    },
+    rs = {
+        build = "cargo build",
+        run = "cargo run"
+    },
+    go = {
+        build = "go build .",
+        run = "go run ."
+    },
+    java = {
+        build = "gradle build",
+        run = "gradle run"
     },
 }
 
+cb:setup {
+    actions = {
+        { name = "run",   keybind = { mode = "n", binding = "<leader>rr" } },
+        { name = "build", keybind = { mode = "n", binding = "<leader>rc" } },
+    },
+    commands = commands,
 
-vim.keymap.set("n", "<leader>rr", cb.run)
-vim.keymap.set("n", "<leader>rc", cb.build)
+    term = {
+        no_number = true,
+    },
+}
