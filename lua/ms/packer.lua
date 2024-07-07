4330bdb0885c6c2a5f0634befb31c59f4f0e19a3
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-07-07 23:06:11 +0200

    get rid of neorg

--- lua/ms/packer.lua
+++ lua/ms/packer.lua
@@ -76,17 +76,6 @@ return packer.startup(function(use)
         }
     }
 
-    -- note taking
-    use {
-        "nvim-neorg/neorg",
-        run = ":Neorg sync-parsers",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-neorg/neorg-telescope",
-        },
-    }
-
     -- LSP
     use {
         "neovim/nvim-lspconfig",
