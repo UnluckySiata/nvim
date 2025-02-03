ea41bbe6664d8e8c445c90e1e8ebd317beca3a7a
Mikołaj Siatkowski <msiatkowski@keemail.me>
2025-02-03 11:25:26 +0100

    config for ai assistants

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,75 @@
+return {
+    {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      "zbirenbaum/copilot.lua",
+      "nvim-lua/plenary.nvim",
+    },
+    build = "make tiktoken",
+    config = function ()
+     local chat = require("CopilotChat")
+     chat.setup {
+       model = "o1",
+     }
+     vim.keymap.set("n", "<leader>ac", chat.toggle)
+    end
+  },
+  {
+    "yetone/avante.nvim",
+    enabled = true,
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      -- provider = "copilot",
+      provider = "ollama",
+      copilot = {
+        endpoint = "https://api.githubcopilot.com",
+        model = "claude-3.5-sonnet",
+        proxy = nil,
+        allow_insecure = false,
+        timeout = 20000,
+        temperature = 0.1,
+        max_tokens = 16384,
+      },
+      vendors = {
+        ollama = {
+          __inherited_from = "openai",
+          api_key_name = "",
+          endpoint = "http://127.0.0.1:11434/v1",
+          model = "deepseek-r1",
+        },
+      },
+    },
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+
+      "echasnovski/mini.icons",
+      "zbirenbaum/copilot.lua",
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  }
+}
